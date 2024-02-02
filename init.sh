2750de31b5750e400a93ab81657eb131bd499520
zoushichen <swei0707@gmail.com>
2024-02-02 23:18:17 +0800

    save job! 2024/02/02-23:18:17

--- init.sh
+++ init.sh
@@ -2,3 +2,4 @@ read -p "Please input your name: " name
 git config --global user.email "$name@aeu.edu.cn"
 git config --global user.name "$name"
 git checkout assignment
+git push
---
+++ save.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+curr_date=`date +%Y/%m/%d-%H:%M:%S`
+git commit -m "save job! $curr_date"
+
